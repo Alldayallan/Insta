80112a6a8de88e2288f7531a3f465e6b87737e85
Allan <mail@allanreid.co>
2021-02-13 12:20:42 -0800

    Initial Commit

---
+++ Insta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Insta
+//
+//  Created by Allan Reid on 2/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
