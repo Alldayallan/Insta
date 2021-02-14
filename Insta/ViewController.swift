8bc700a7d8ec506797fe4f7564b800e23d208ac7
Allan <mail@allanreid.co>
2021-02-14 15:52:01 -0800

    Upload photos

--- Insta/ViewController.swift
+++ Insta/ViewController.swift
@@ -12,16 +12,29 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var emailText: UITextField!
     
-    
     @IBOutlet weak var passwordText: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
     @IBAction func signInClicked(_ sender: Any) {
-        performSegue(withIdentifier: "toFeedVC", sender: nil)
+        
+        if emailText.text != "" && passwordText.text != "" {
+            
+            Auth.auth().signIn(withEmail: emailText.text!, password: passwordText.text!) { (authdata, error) in
+                if error != nil {
+                    self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error")
+                } else {
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
+            
+        } else {
+            makeAlert(titleInput: "Error", messageInput: "Username/Password?")
+        }
     }
     
     @IBAction func signUpClicked(_ sender: Any) {
